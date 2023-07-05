eca445ec7a36cab325932b7db321bf80fcc58e94
keith duncan <duncanclanof4@gmail.com>
2023-07-05 14:28:12 -0400

    Merge models together

---
+++ TebucksServer/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TEbucksServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+    }
+}
---
+++ TebucksServer/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace TEbucksServer.Models
+{
+    public class Account
+    {
+        public int accountId { get; set; }
+        public int user_Id { get; set; }
+        public decimal balance { get; set; }
+    }
+}
