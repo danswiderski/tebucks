36cc8c8df757d05e236f47823acfbccacbb6fc39
keith duncan <duncanclanof4@gmail.com>
2023-07-05 18:20:09 -0400

    Finished the Account DAO and Controller.  I left TODO notes for you.

---
+++ TebucksServer/DAO/IAccountDAO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TEbucksServer.Models;
+
+namespace TEbucksServer.DAO
+{
+    public interface IAccountDAO
+    {
+        Account GetAccount(int accountId);
+
+        Account GetAccountByUserName(string username);
+        List<Transfer> GetAccountTransfer(string username);
+    }
+}
