bfa10502bb4881fd6901e098402a9db57e9cb672
Dan Swiderski <nutmegpunk77@gmail.com>
2023-07-05 14:23:25 -0400

    testing

---
+++ TebucksServer/Models/Transfer.cs
@@ -0,0 +1,33 @@
+﻿using TEBucksServer.Models;
+
+namespace TEbucksServer.Models
+{
+    public class Transfer
+    {
+        public int TransferID { get; set; }
+
+        public int TransferType { get; set; }
+
+        public int TransferStatus { get; set; }
+
+        public User UserFrom { get; set; }
+
+        public User UserTo { get; set; }
+
+        public decimal Amount { get; set; }
+
+    }
+
+    public class NewTransfer
+    {
+        public int UserFrom { get; set; }
+        public int UserTo { get; set; }
+        public decimal Amount { get; set; }
+        public string TransferType { get; set; }
+
+    }
+    public class TransferStatus
+    {
+        public string TransferStatusUpdate { get; set; }
+    }
+}
